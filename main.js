bb0e594c528e6478c4207bbcd9234f2fb9cefe87
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-02-04 16:06:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function nextFrame(timeStamp) {
 			game.timewallPoint++;
 		}
 		document.getElementById("progressBar").value = game.progress;
-		document.getElementById("progressBarLabel").innerHTML = (game.progress / document.getElementById("progressBar").max).toFixed(10) + "%";
+		document.getElementById("progressBarLabel").innerHTML = (game.progress / document.getElementById("progressBar").max).toFixed(6) + "%";
 	}
 	if (sinceLastSave >= game.autoSaveSpeed) {
 		if (game.doAutoSave) {
