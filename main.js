d59bf788881daf3c987938c73da5adb87768dd44
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-02-07 12:11:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function nextFrame(timeStamp) {
 	let sinceLastSave = timeStamp - lastSave;
 	if (sinceLastFrame >= game.updateSpeed) {
 		lastFrame = timeStamp;
-		game.lifetimeProgress += Math.pow(sinceLastFrame * getBarSpeed(), 1 / (game.progress < getBarLength() ? 1 : 2));
-		game.progress += Math.pow(sinceLastFrame * getBarSpeed(), 1 / (game.progress < getBarLength() ? 1 : 2));
+		game.lifetimeProgress += Math.pow(sinceLastFrame * getBarSpeed(), 1 / (game.progress < getBarLength() ? 1 : 4));
+		game.progress += Math.pow(sinceLastFrame * getBarSpeed(), 1 / (game.progress < getBarLength() ? 1 : 4));
 		updateProgress();
 	}
 	if (sinceLastSave >= game.autoSaveInterval) {
