f601dae040b0fad93ad4c97e27bf2d861de3be8b
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-03-10 10:21:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -226,7 +226,15 @@ function redeemPoints(n) {
 	if (game.progress[n] >= getBarLength(n)) {
 		game.points[n] += getPointGain(n);
 		game.lifetimePoints[n] += getPointGain(n);
-		game.progress[n] = game.progress[n] % getBarLength(n);
+		if (n == 1 && game.upgrade.normal[7]) {
+			game.progress[n] = game.progress[n] % getBarLength(n);
+		} else {
+			game.progress[0] = 0;
+			game.points[0] = 0;
+			for (let i = 0; i < 4; i++) {
+				game.upgrade.normal[i] = 0;
+			}
+		}
 		updatePoints();
 		updateUpg();
 	}
