d35d9112c6170e608cd212f53c0fb72845b07857
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-04-23 13:05:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -424,7 +424,7 @@ function updateSkills() {
 	let line = document.getElementById("sinGraphLine");
 	let percent = (1 - Math.sin(skill.sinDuration / 250)) / 2;
 	line.style.top = percent * 100 + "%";
-	line.style.backgroundColor = "rgb(" + (255*Math.pow(percent,0.2)) + "," + (255*(1-Math.pow(percent,0.2))) + ",0)";
+	line.style.backgroundColor = "rgb(" + (255*Math.pow(percent,0.2)) + "," + (255*Math.pow(1-percent,0.2)) + ",0)";
 }
 
 function maxAll(type = "normal") {
