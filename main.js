d9b4e046fe97a5ec6670495c8c6e22922cd65ae3
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-04-27 09:51:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -349,7 +349,7 @@ function updateProgress() {
 	document.getElementById("switchScreenRight").classList[game.lifetimeProgress[1] >= getBarLength(1)/100 ? "remove" : "add"]("hidden");
 	if (game.upgrade.normal[7] == 0) {
 		game.progress[1] = game.progress[0] == 0 ? 0 : Math.log10(game.progress[0] == Infinity ? 1.79e308 : game.progress[0]);
-		if (game.progress[1] > game.lifetimeProgress[1]) game.lifetimeProgress[1] = game.progress;
+		if (game.progress[1] > game.lifetimeProgress[1]) game.lifetimeProgress[1] = game.progress[1];
 	}
 }
 
