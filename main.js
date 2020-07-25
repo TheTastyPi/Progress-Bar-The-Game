80ec7f587db5b71e0aba9e35342e04d0a6b7dc9f
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-07-25 17:40:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ function doFrame(sinceLastFrame) {
 						document.body.appendChild(coupon);
 						coupon.id = "coupon";
 						coupon.classList.add("coupon");
-						coupon.style.transform = "rotate("+Math.random()*40-20+"deg) translate(calc("+Math.random()+"*(100vw-248px)),"+Math.random()+"*(100vh-77px)))";
+						coupon.style.transform = "rotate("+(Math.random()*40-20)+"deg) translate(calc("+Math.random()+"*(100vw-248px)),"+Math.random()+"*(100vh-77px)))";
 						coupon.onclick = "couponClick();";
 					}
 					if (game.skill.couponTimer > 0) {
