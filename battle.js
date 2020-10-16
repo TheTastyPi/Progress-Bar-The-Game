062bbef472d9eefbdb23e4db8fae299dd8bc3a82
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-10-16 11:28:27 +0800

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,6 +1,77 @@
+const baseAttackPower = [5, 5, 30, 50];
+const baseAttackCost = [0, 10, 20, 40];
 const effectList = ["overpump","underpump","autodrain","autofill","accelerate","decelerate"];
-const enemyList = {
-	test: {
-		
+const areaList = [];
+const enemyList = [];
+
+var areaIdCount = 0;
+class Area {
+	constructor(name, spawnType, spawnRate) {
+		this.name = name;
+		this.spawnType = spawnType;
+		this.spawnRate = spawnRate;
+		areaList[areaIdCount] = this;
+		areaIdCount++;
 	}
-};
+}
+new Area("Obligatory Safe Zone", [0], Infinity);
+new Area("Weak Area", [1], 4000);
+
+var enemyIdCount = 1;
+class Enemy {
+	constructor(name, maxHP, str, def, cooldown, xp, effType = [], effChance = [], effSelf = [], effSelfChance = [], special = function(){}) {
+		this.name = name;
+		this.maxHP = maxHP;
+		this.str = str;
+		this.def = def;
+		this.cooldown = cooldown;
+		this.effType = effType;
+		this.effChance = effChance;
+		this.effSelf = effSelf;
+		this.effSelfChance = effSelfChance;
+		this.special = special;
+		enemyList[enemyIdCount] = this;
+		currentEnemyId++;
+	}
+}
+new Enemy("test", 100, 5, 0, 3000, 1);
+function getPlayerLevel() {
+	let lvl = 1 + Math.floor(Math.sqrt(game.battle.xp / 5));
+	return lvl;
+}
+function getPlayerStrength() {
+	let player = game.battle.player;
+	let str = Math.sqrt(getPlayerLevel()) * (1 + 0.2*(player.effLevel[0]-player.effLevel[1]));
+	return str;
+}
+function getPlayerMaxHP() {
+	let maxHP = 80 + 20 * getPlayerLevel();
+	return maxHP;
+}
+function getPlayerMaxSP() {
+	let maxSP = 45 + 5 * getPlayerLevel();
+	return maxSP;
+}
+function getPlayerDamage(power) {
+	let enemy = enemyList[game.battle.currentEnemy];
+	let dmg = Math.floor(power * getPlayerStrength()) - enemy.def;
+	return dmg;
+}
+function getAttackCost(type) {
+	let cost = baseAttackCost[type];
+	return cost;
+}
+function playerAttack(type) {
+	let player = game.battle.player;
+	let enemy = enemyList[game.battle.currentEnemy];
+	if (player.sf >= getAttackCost(type)) {
+		player.sf -= getAttackCost(type);
+		enemy.hp -= getPlayerDamage(baseAttackPower[type]);
+		if (type == 1) {
+			enemy.hp -= getPlayerDamage(baseAttackPower[type]);
+			enemy.hp -= getPlayerDamage(baseAttackPower[type]);
+		}
+		if (enemy.hp <= 0) game.battle.currentEnemy = 0;
+		updateBattle();
+	}
+}
