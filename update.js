0d3d3829601945f5a79ad4fa9b5f176fefb37555
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-11-19 11:38:12 +0800

    Update update.js

--- update.js
+++ update.js
@@ -339,6 +339,7 @@ function updateBattleInv() {
 	for (let i in game.battle.inventory) {
 		if (game.battle.inventory[i] == 0) {
 			id("battleInvSpace"+i).style.backgroundImage = "";
+			id("battleInvSpace"+i).style.backgroundColor = "#DDDDDD";
 		} else {
 			id("battleInvSpace"+i).style.backgroundImage = "url('pics/battle/item" + game.battle.inventory[i].id + "')";
 			id("battleInvSpace"+i).style.backgroundColor = "#222222";
