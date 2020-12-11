8d0d185b2e5b0844cc67247a93ac70b9a2133106
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2020-12-11 10:50:56 +0800

    Update update.js

--- update.js
+++ update.js
@@ -354,6 +354,8 @@ function updateBattleInv() {
 					let propDisp = item[propPriorityList[i]];
 					if (propPriorityList[i] == "type") {
 						propDisp = itemGroupList[propDisp[0]] + "/" + itemTypeList[propDisp[0]][propDisp[1]];
+					} else if (typeof(propDisp) == "number") {
+						propDisp = format(propDisp,2);
 					}
 					tooltipText += propDispList[i] + ": " + propDisp + (propPriorityList[i] == "modifiersLeft" ? "<hr>" : "<br>");
 				}
